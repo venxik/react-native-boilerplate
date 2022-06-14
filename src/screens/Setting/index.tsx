27b64f420f15a836ebf5d30e0c23aa6dcfcbadf3
Kholiq <kholiqcode@gmail.com>
2022-06-15 02:33:08 +0700

    feat: setup crashlytics for ios and android

--- src/screens/Setting/index.tsx
+++ src/screens/Setting/index.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import crashlytics from '@react-native-firebase/crashlytics';
+import { Button, Text, View } from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native';
 
-export default function Setting(): JSX.Element {
+export default function Setting() {
+  const [count, setCount] = useState(0);
+
+  const onIncrementWNonFatal = () => {
+    crashlytics().log('call onIncrementWNonFatal.');
+    try {
+      if (count >= 4) {
+        setCount(count.push(count.length));
+      }
+      setCount(count + 1);
+    } catch (error: Error) {
+      crashlytics().recordError(error);
+    }
+  };
+
+  const onIncrementWFatal = () => {
+    crashlytics().log('call onIncrementWFatal.');
+    try {
+      if (count >= 4) {
+        crashlytics().crash();
+        setCount(count + 1);
+        throw new Error(count + ' greater than or equal five ');
+      }
+      setCount(count + 1);
+    } catch (error: Error) {
+      crashlytics().recordError(error);
+    }
+  };
+
+  useEffect(() => {
+    crashlytics().log('Setting screen mounted.');
+  }, []);
   return (
-    <View>
-      <Text>Setting</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, padding: 20 }}>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text style={{ color: 'red', textAlign: 'center' }} fontSize={40}>
+          {count}
+        </Text>
+      </View>
+      <Button onPress={onIncrementWNonFatal}>Increment + Non-Fatal</Button>
+      <Button onPress={onIncrementWFatal} style={{ marginTop: 10 }}>
+        Increment + Fatal
+      </Button>
+      <Button bgColor={'red.500'} style={{ marginTop: 10 }} onPress={() => setCount(0)}>
+        Reset
+      </Button>
+    </SafeAreaView>
   );
 }
