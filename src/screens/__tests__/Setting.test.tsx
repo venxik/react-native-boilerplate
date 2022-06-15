18f3b961bee44853690f19584867d6f036f7e0cb
Kholiq <kholiqcode@gmail.com>
2022-06-16 04:04:42 +0700

    revert: fix remote config test

--- src/screens/__tests__/Setting.test.tsx
+++ src/screens/__tests__/Setting.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import Setting from '../Setting';
+import { wrapper } from '../../__mocks__/wrapper';
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({ navigate: mockedNavigate }),
+}));
 
 describe('Setting screen', () => {
   it('Render Setting screen', () => {
-    const setting = render(<Setting />);
+    const setting = render(<Setting />, { wrapper });
     expect(setting).toBeTruthy();
   });
 });
