9a1fef47044a6a7b5120bba2d5402fcf630bab42
Kevin <44430202+venxik@users.noreply.github.com>
2022-06-23 21:51:16 +0700

    fix: update

--- scripts/run-e2e-android-release.sh
+++ scripts/run-e2e-android-release.sh
@@ -6,7 +6,7 @@ METRO_BUNDLER_PID=$!
 
 npx detox clean-framework-cache && npx detox build-framework-cache
 
-yarn test:e2e-android-dev-debug --headless
+yarn test:e2e-android-dev-debug --headless --debug-synchronization 200
 
 DETOX_EXIT_CODE=$?
 
